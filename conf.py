ac20f49e9bc67efa07398d5bf17d05c1c08cb16a
dirif25non-partage <156591976+dirif25non-partage@users.noreply.github.com>
2024-01-16 19:02:42 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,1 +1,25 @@
-extensions = ['myst_parser']
+project = 'doc Sucombe'
+copyright = '2043, on'
+author = 'on'
+release = ''
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_theme = 'furo'
+html_static_path = ['_static']
+html_show_copyright = False
+html_show_sphinx = False
+html_theme_options = {
+    "sidebar_hide_name": True,
+    "light_logo": "logo.svg",
+    "dark_logo": "logo.svg",
+}
