10a7e269b21837b2dba843f6f97f5825dd9563c6
dirif25non-partage <156591976+dirif25non-partage@users.noreply.github.com>
2024-01-17 08:26:43 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -21,6 +21,6 @@ html_show_copyright = False
 html_show_sphinx = False
 html_theme_options = {
     "sidebar_hide_name": True,
-    "light_logo": "logoDETT.jpg",
-    "dark_logo": "logoDETT.jpg",
+    "light_logo": "logoMTECTTEM.jpg",
+    "dark_logo": "logoMTECTTEM.jpg",
 }
