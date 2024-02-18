a49ee7b67c3bcfdbf4dda23d0f3d7265342cdb5d
dirif25non-partage <156591976+dirif25non-partage@users.noreply.github.com>
2024-02-18 16:46:45 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -15,7 +15,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 html_theme = 'furo'
-html_style = 'css/custom.css'
+# html_style = 'css/custom.css'
 html_favicon = 'https://storage.googleapis.com/idf-congestion/favicon.ico'
 html_static_path = ['_static']
 html_css_files = [    'css/custom.css',]
